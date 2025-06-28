6ffefc42209a431ccf48040681bdd0d16cabb520
MD RAHADUL ISLAM <riyadrahadulislam3@gmail.com>
2025-06-28 17:17:04 +0600

    Add files via upload

---
+++ Insertion Sort.cpp
@@ -0,0 +1,25 @@
+//Insertion Sort
+#include iostream
+using namespace std;
+void insertionSort(int A[], int n) {
+    for (int i=1; in; i++) {
+        int key = A[i];
+
+        int j=i-1;
+        while (j=0 && A[j]key) {
+            A[j+1] = A[j];
+            j = j-1;
+        }
+        A[j+1] = key;
+    }
+}
+int main() {
+    int A[] = {9, 5, 1, 4, 3};
+    int n = sizeof(A)sizeof(A[0]);
+    insertionSort(A, n);
+    coutSorted array ;
+    for (int i=0; in; i++)
+        coutA[i] ;
+    coutendl;
+    return 0;
+}
